652b59e7a44b7efc2700afc488de830544274aa5
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-11-28 05:59:09 +0700

    update homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -12,9 +13,26 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Train Getx 2"),
+        title: Text(
+          "Train Getx 2",
+          style: GoogleFonts.poppins(),
+        ),
         backgroundColor: Colors.green.shade800,
         centerTitle: true,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Increment counter: 0"),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Add"),
+            ),
+          ],
+        ),
       ),
     );
   }
