a8c28d47ccce5872c66887a6d0a05f3be8984864
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-11-29 11:33:04 +0700

    update controller

--- lib/increment_controller.dart
+++ lib/increment_controller.dart
@@ -1,15 +1,22 @@
 import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
 
-class IncrementController extends GetxController {
+class IncreDecreController extends GetxController {
   var count1 = 0.obs;
   int count2 = 0;
 
 
   increment1() => count1.value++;
 
+  decrement1() => count1.value--;
+
   increment2(){
     count2++;
+    update();
+  }
+
+  decrement2(){
+    count2--;
     update();
   }
 }
