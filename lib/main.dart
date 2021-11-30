3d3c1a2331b6457a2997df1bec831d550b814ec1
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-11-30 14:35:17 +0700

    updaet main dart: add screenutil and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_2/apptheme_controller.dart';
+import 'package:flutter_getx_2/constants.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'homepage.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+
+  bool isChanged = false;
+
+  AppThemeController appThemeController = Get.put(AppThemeController());
+  
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: HomePage(),
-      title: 'Train Getx 2',
-      debugShowCheckedModeBanner: false,
+    appThemeController.activatedTheme();
+    return ScreenUtilInit(
+      builder: () {
+        return GetMaterialApp(
+          home: const HomePage(),
+          title: 'Train Getx 2',
+          debugShowCheckedModeBanner: false,
+          theme: appThemeData,
+        );
+      },
     );
   }
 }
