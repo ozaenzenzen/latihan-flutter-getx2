0c18c0fdfefd6f9d74e822210819e485eb12cfb2
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-12-04 09:21:10 +0700

    update app theme controller

--- lib/apptheme_controller.dart
+++ lib/apptheme_controller.dart
@@ -1,10 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:math';
 
-class AppThemeController extends GetxController{
+class AppThemeController extends GetxController {
   bool isActive = true;
 
-  void activatedTheme(){
-    isActive = !isActive;
+  List<Color> appColor = [
+    Colors.pink,
+    Colors.red,
+    Colors.teal,
+    Colors.purpleAccent,
+    Colors.orange,
+  ];
+
+  Rx<Color> currentColor = Colors.blue.obs;
+
+  var base = 0;
+
+  Random random = Random();
+
+  void activatedTheme() {
+    currentColor = Rx<Color>(appColor[random.nextInt(5)]);
+    // base = RxInt(random.nextInt(5));
     update();
   }
 }
