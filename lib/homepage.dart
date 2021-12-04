426de288c2d2f3e9c2bae4dda420745de96b2041
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-12-04 09:21:16 +0700

    update page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_2/apptheme_controller.dart';
 import 'package:flutter_getx_2/incredecre_controller.dart';
 import 'package:flutter_getx_2/secondpage.dart';
 import 'package:get/get.dart';
@@ -12,80 +13,109 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final IncreDecreController controller = Get.put(IncreDecreController());
+  IncreDecreController? controller;
+  AppThemeController? appThemeController;
+
+  @override
+  void initState() {
+    controller = Get.put(IncreDecreController());
+    appThemeController = Get.put(AppThemeController());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Train Getx 2"),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Obx: "),
-                Obx(() {
-                  return Text("Increment counter: ${controller.count1.value}");
-                }),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("GetBuilder: "),
-                GetBuilder(
-                  init: IncreDecreController(),
-                  builder: (builderController) =>
-                      Text("Increment counter: ${controller.count2}"),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.green.shade800,
-                  ),
-                  onPressed: () {
-                    controller.increment1();
-                    controller.increment2();
-                  },
-                  child: const Text("Add"),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.green.shade800,
-                  ),
-                  onPressed: () {
-                    controller.decrement1();
-                    controller.decrement2();
-                  },
-                  child: const Text("Subtract"),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green.shade800,
-              ),
-              onPressed: () {
-                Get.to(() => const SecondPage());
-              },
-              child: const Text("Next Page"),
-            ),
-          ],
+    return Obx(() {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Train Getx 2"),
+          backgroundColor: appThemeController!.currentColor.value,
         ),
-      ),
-    );
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Obx: "),
+                  Obx(() {
+                    return Text(
+                        "Increment counter: ${controller!.count1.value}");
+                  }),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("GetBuilder: "),
+                  GetBuilder(
+                    init: IncreDecreController(),
+                    builder: (builderController) =>
+                        Text("Increment counter: ${controller!.count2}"),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green.shade800,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        controller!.increment1();
+                        controller!.increment2();
+                      });
+                    },
+                    child: const Text("Add"),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green.shade800,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        controller!.decrement1();
+                        controller!.decrement2();
+                      });
+                    },
+                    child: const Text("Subtract"),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: appThemeController!.currentColor.value,
+                  // primary: Colors.green.shade800,
+                ),
+                onPressed: () {
+                  setState(() {
+                    appThemeController!.activatedTheme();
+                  });
+                },
+                child: const Text("Change AppBar Color"),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green.shade800,
+                ),
+                onPressed: () {
+                  Get.to(() => const SecondPage());
+                },
+                child: const Text("Next Page"),
+              ),
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_2/apptheme_controller.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class SecondPage extends StatefulWidget {
   const SecondPage({Key? key}) : super(key: key);
@@ -8,12 +11,19 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  ScreenUtil screenUtil = ScreenUtil();
+  // final appThemeController = Get.put(AppThemeController());
+  final appThemeController = Get.find();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Second Page"),
-      ),
-    );
+    return Obx(() {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Second Page"),
+          backgroundColor: appThemeController.currentColor.value,
+        ),
+      );
+    });
   }
 }
