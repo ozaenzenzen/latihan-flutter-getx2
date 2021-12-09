215d99eba6a9fa1762d19593798680d61f06dff1
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-12-09 10:44:28 +0700

    update secondpage: elev 0, add controller

--- lib/page/secondpage.dart
+++ lib/page/secondpage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_getx_2/controller/apptheme_controller.dart';
 import 'package:flutter_getx_2/controller/incredecre_controller.dart';
+import 'package:flutter_getx_2/controller/product_controller.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
@@ -18,6 +19,7 @@ class _SecondPageState extends State<SecondPage> {
 
   IncreDecreController controller = Get.find<IncreDecreController>();
   AppThemeController appThemeController = Get.find<AppThemeController>();
+  ProductController productController = Get.find<ProductController>();
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +28,16 @@ class _SecondPageState extends State<SecondPage> {
         appBar: AppBar(
           title: const Text("Second Page"),
           backgroundColor: appThemeController.currentColor.value,
+          elevation: 0,
+        ),
+        body: ListView.builder(
+          itemCount: productController.items.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text("${productController.items[index].name}"),
+              subtitle: Text("${productController.items[index].description}"),
+            );
+          },
         ),
       );
     });
