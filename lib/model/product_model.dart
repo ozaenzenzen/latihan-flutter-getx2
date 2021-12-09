2d9529f4b1ef363fc0f60a4da7a4b3da70434165
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-12-09 10:43:36 +0700

    add product model

---
+++ lib/model/product_model.dart
@@ -0,0 +1,13 @@
+class ProductModel {
+  final String? id;
+  final String? name;
+  final String? description;
+  final String? picture;
+
+  ProductModel({
+    this.id,
+    this.name,
+    this.description,
+    this.picture,
+  });
+}
