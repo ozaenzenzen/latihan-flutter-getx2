ce21a198b8037aaa0d71366e264dfc9bdd02f8a8
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-12-13 06:48:50 +0700

    update product controller: add mockdata

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -1,7 +1,23 @@
 import 'package:flutter_getx_2/model/product_model.dart';
 import 'package:get/state_manager.dart';
+import 'package:mock_data/mock_data.dart';
 
 class ProductController extends GetxController {
+  final List<ProductModel> data = [];
+
+  ProductController() {
+    for (var i = 0; i < 20; i++) {
+      data.add(
+        ProductModel(
+          id: "${mockInteger(1, 6)}",
+          name: mockName('male'),
+          description: mockString(10),
+          picture: mockString(20),
+        ),
+      );
+    }
+  }
+
   final List<ProductModel> _items = [
     ProductModel(
       id: "0s0s0s0",
@@ -105,9 +121,9 @@ class ProductController extends GetxController {
       description: "ss",
       picture: "ssss",
     ),
-  ].obs;
+  ];
 
-  get items{
+  get items {
     return [..._items].obs;
   }
 }
