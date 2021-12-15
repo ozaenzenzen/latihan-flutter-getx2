a55c184172442734cff3556a9a6c8410676ef564
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-12-15 07:04:24 +0700

    update profile services

--- lib/services/profile_services.dart
+++ lib/services/profile_services.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: avoid_print
+
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_getx_2/model/profile_model.dart';
+import 'package:http/http.dart' as http;
 
 class ProfileServices {
   ProfileServices._();
@@ -13,11 +16,32 @@ class ProfileServices {
   Dio dio = Dio();
 
   Future getDataProfile() async {
-    Response response = await dio.get(url);
+    Response response;
+    try {
+      response = await dio.get(url);
 
-    Map jsonData = jsonDecode(response.data);
+      return response.data;
 
-    return jsonData;
-    // return response.data;
+      // List listUser =
+      //     (response.data as List).map((e) => ProfileModel.fromJson(e)).toList();
+      // // print(listUser);
+
+      // return listUser;
+    } catch (e) {
+      print(e);
+    }
+
+    // try {
+    //   var response2 = await http.get(Uri.parse(url));
+    //   // print(response2.body);
+
+    //   List jsonObject = json.decode(response2.body);
+    //   // print(jsonObject);
+
+    //   List listUser = jsonObject.map((e) => ProfileModel.fromJson(e)).toList();
+    //   // print(listUser);
+    // } catch (e) {
+    //   print(e);
+    // }
   }
 }
